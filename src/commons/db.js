c465fa6dd32239d79e4512087af0dd43b7d70ef6
Ashish Sahani <developer.ashish.sahani@gmail.com>
2023-03-29 14:02:03 +0530

    Updated for using single docker file

--- src/commons/db.js
+++ src/commons/db.js
@@ -1,7 +1,7 @@
 const mongoose =require('mongoose');
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    await mongoose.connect('mongodb://db:27017/merndb');
   
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
